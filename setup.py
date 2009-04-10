964b58cb9cc99c54f9bce6c26601f310ca469cc7
Pavol Juhas <pavol.juhas@gmail.com>
2009-04-10 22:45:53 +0000

    Nudged up the minimum required versions of dependencies.

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         scripts = scripts,
         ext_modules = [pdffit2module],
         install_requires = [
-            'diffpy.Structure>=1.0c1.dev-r2824',
+            'diffpy.Structure>=1.0-r3050',
         ],
         dependency_links = [
             'http://www.diffpy.org/packages/',
