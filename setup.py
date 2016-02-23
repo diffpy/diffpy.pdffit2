6e6f443adc884c6f8debde963d10829f5c5c325e
Pavol Juhas <pavol.juhas@gmail.com>
2016-02-23 15:21:36 -0500

    Version 1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import Extension
 
 # Use this version when git data are not available, like in git zip archive.
 # Update when tagging a new release.
-FALLBACK_VERSION = '1.1a1.post0'
+FALLBACK_VERSION = '1.1.post0'
 
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
