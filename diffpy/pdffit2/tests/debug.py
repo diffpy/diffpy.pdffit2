d5a53b0582d0f40e6b45af131a409e7fd06d2680
Pavol Juhas <pavol.juhas@gmail.com>
2016-09-28 13:57:11 -0400

    Convenience script for debugging unit tests.

---
+++ diffpy/pdffit2/tests/debug.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.pdffit2    Complex Modeling Initiative
+#                   (c) 2016 Brookhaven Science Associates,
+#                   Brookhaven National Laboratory.
+#                   All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+
+"""\
+Convenience module for debugging the unit tests using
+
+python -m diffpy.pdffit2.tests.debug
+
+Exceptions raised by failed tests or other errors are not caught.
+"""
+
+
+if __name__ == '__main__':
+    from diffpy.pdffit2.tests import testsuite
+    suite = testsuite()
+    suite.debug()
+
+
+# End of file
