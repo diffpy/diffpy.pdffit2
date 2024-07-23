7f6ca7cd7e4ba4f879dd4133634c7bbc33b27bf4
Sam Andrello <samqandrello@gmail.com>
2024-07-23 12:30:12 -0400

    pre-commit fixes

--- src/diffpy/pdffit2/tests/__init__.py
+++ src/diffpy/pdffit2/tests/__init__.py
@@ -20,8 +20,9 @@
 
 import unittest
 
-def testsuite(pattern=''):
-    '''Create a unit tests suite for the diffpy.pdffit2 package.
+
+def testsuite(pattern=""):
+    """Create a unit tests suite for the diffpy.pdffit2 package.
 
     Parameters
     ----------
@@ -34,30 +35,31 @@ def testsuite(pattern=''):
     -------
     suite : `unittest.TestSuite`
         The TestSuite object containing the matching tests.
-    '''
+    """
     import re
-    from os.path import dirname
     from itertools import chain
+    from os.path import dirname
+
     from pkg_resources import resource_filename
+
     loader = unittest.defaultTestLoader
-    thisdir = resource_filename(__name__, '')
-    depth = __name__.count('.') + 1
+    thisdir = resource_filename(__name__, "")
+    depth = __name__.count(".") + 1
     topdir = thisdir
     for i in range(depth):
         topdir = dirname(topdir)
-    suite_all = loader.discover(thisdir, pattern='*Test*.py',
-                                top_level_dir=topdir)
+    suite_all = loader.discover(thisdir, pattern="*Test*.py", top_level_dir=topdir)
     # always filter the suite by pattern to test-cover the selection code.
     suite = unittest.TestSuite()
     rx = re.compile(pattern)
     tsuites = list(chain.from_iterable(suite_all))
     tsok = all(isinstance(ts, unittest.TestSuite) for ts in tsuites)
-    if not tsok:    # pragma: no cover
+    if not tsok:  # pragma: no cover
         return suite_all
     tcases = chain.from_iterable(tsuites)
     for tc in tcases:
-        tcwords = tc.id().split('.')
-        shortname = '.'.join(tcwords[-3:])
+        tcwords = tc.id().split(".")
+        shortname = ".".join(tcwords[-3:])
         if rx.search(shortname):
             suite.addTest(tc)
     # verify all tests are found for an empty pattern.
@@ -66,12 +68,12 @@ def testsuite(pattern=''):
 
 
 def test():
-    '''Execute all unit tests for the diffpy.pdffit2 package.
+    """Execute all unit tests for the diffpy.pdffit2 package.
 
     Returns
     -------
     result : `unittest.TestResult`
-    '''
+    """
     suite = testsuite()
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
@@ -79,17 +81,18 @@ def test():
 
 
 def testdeps():
-    '''Execute all unit tests for diffpy.pdffit2 and its dependencies.
+    """Execute all unit tests for diffpy.pdffit2 and its dependencies.
 
     Returns
     -------
     result : `unittest.TestResult`
-    '''
+    """
     from importlib import import_module
-    modulenames = '''
+
+    modulenames = """
         diffpy.pdffit2.tests
         diffpy.structure.tests
-    '''.split()
+    """.split()
     suite = unittest.TestSuite()
     for mname in modulenames:
         mod = import_module(mname)
