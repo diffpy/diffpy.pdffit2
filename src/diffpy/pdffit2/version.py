29d68a277e06e4ee5b6d04c8e5159a230f8cc4cf
Sam Andrello <samqandrello@gmail.com>
2024-07-23 12:25:08 -0400

    add other vars in version.py

--- src/diffpy/pdffit2/version.py
+++ src/diffpy/pdffit2/version.py
@@ -16,11 +16,14 @@
 """Definition of __version__."""
 
 #  We do not use the other three variables, but can be added back if needed.
-#  __all__ = ["__date__", "__git_commit__", "__timestamp__", "__version__"]
+__all__ = ["__date__", "__git_commit__", "__timestamp__", "__version__"]
 
 # obtain version information
 from importlib.metadata import version
 
 __version__ = version("diffpy.pdffit2")
+__date__ = __all__[0]
+__git_commit__ = __all__[1]
+__timestamp__ = __all__[2]
 
 # End of file
