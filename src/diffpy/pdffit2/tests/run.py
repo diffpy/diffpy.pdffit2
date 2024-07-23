e77515def0e4e488648e4450e315f750985bf214
Sam Andrello <samqandrello@gmail.com>
2024-07-23 12:09:54 -0400

    run.py without pytest

--- src/diffpy/pdffit2/tests/run.py
+++ src/diffpy/pdffit2/tests/run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 ##############################################################################
 #
+# (c) 2012 Trustees of the Columbia University in the City of New York.
+# All rights reserved.
 # (c) 2024 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
@@ -12,23 +14,39 @@
 # See LICENSE.rst for license information.
 #
 ##############################################################################
+
 """Convenience module for executing all unit tests with
 python -m diffpy.pdffit2.tests.run
 """
 
-import sys
 
-import pytest
+if __name__ == '__main__':
+    import sys
+    # show warnings by default
+    if not sys.warnoptions:
+        import os, warnings
+        warnings.simplefilter("default")
+        # also affect subprocesses
+        os.environ["PYTHONWARNINGS"] = "default"
+    from diffpy.pdffit2.tests import test
+    # produce zero exit code for a successful test
+    sys.exit(not test().wasSuccessful())
 
-if __name__ == "__main__":
-    # show output results from every test function
-    args = ["-v"]
-    # show the message output for skipped and expected failure tests
-    if len(sys.argv) > 1:
-        args.extend(sys.argv[1:])
-    print("pytest arguments: {}".format(args))
-    # call pytest and exit with the return code from pytest
-    exit_res = pytest.main(args)
-    sys.exit(exit_res)
+
+# Consider upgrading to pytest
+# import sys
+#
+# import pytest
+#
+# if __name__ == "__main__":
+#     # show output results from every test function
+#     args = ["-v"]
+#     # show the message output for skipped and expected failure tests
+#     if len(sys.argv) > 1:
+#         args.extend(sys.argv[1:])
+#     print("pytest arguments: {}".format(args))
+#     # call pytest and exit with the return code from pytest
+#     exit_res = pytest.main(args)
+#     sys.exit(exit_res)
 
 # End of file
