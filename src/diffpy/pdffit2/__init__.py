959302bc8db7c79cbc56b3505da63462a14b296e
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-11-19 01:44:40 -0500

    fix: add import from C extension back

--- src/diffpy/pdffit2/__init__.py
+++ src/diffpy/pdffit2/__init__.py
@@ -16,6 +16,20 @@
 ##############################################################################
 """PDFfit2 - real space structure refinement program."""
 
+# WARNING: Do NOT remove the isort: off/on comments in this file.
+# These tags are used to prevent isort from reordering imports in this file.
+# __version__ must be initialized before importing C++ extensions.
+
+# isort: off
+# Import the package version before C++ extensions are loaded.
+from diffpy.pdffit2.output import redirect_stdout  # noqa
+
+# Import C++ related modules since the __version__ attribute is used.
+from diffpy.pdffit2.pdffit import PdfFit  # noqa
+
+
+# isort: on
+
 # package version
 from diffpy.pdffit2.version import __version__  # noqa
 
