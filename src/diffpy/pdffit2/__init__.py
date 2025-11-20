eae357eaf83c94fea2a1fa903b0b167a80a33820
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-11-20 15:26:17 -0500

    skpkg: change version build file.

--- src/diffpy/pdffit2/__init__.py
+++ src/diffpy/pdffit2/__init__.py
@@ -16,24 +16,10 @@
 ##############################################################################
 """PDFfit2 - real space structure refinement program."""
 
-# WARNING: Do NOT remove the isort: off/on comments in this file.
-# These tags are used to prevent isort from reordering imports in this file.
-# __version__ must be initialized before importing C++ extensions.
+# package version
+from diffpy.pdffit2.version import __version__  # noqa
 
-# isort: off
-# Import the package version before C++ extensions are loaded.
-from diffpy.pdffit2.output import redirect_stdout
-from diffpy.pdffit2.version import __date__, __version__
-
-# Import C++ related modules since the __version__ attribute is used.
-from diffpy.pdffit2.pdffit import PdfFit
-from diffpy.pdffit2.pdffit2 import is_element
-
-# isort: on
-
-# Ensure all necessary components are imported and initialized
+# silence the pyflakes syntax checker
 assert __version__ or True
-assert __date__ or True
-assert all((PdfFit, redirect_stdout, is_element))
 
 # End of file
